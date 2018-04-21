b28b78472c6bbedda1c391febc7e5c3fa61c4fc7
An <1512012@student.hcmus.edu.vn>
2018-04-22 05:44:57 +0700

    Add list of the newest products, best seller products and hightest view products

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,12 +1,69 @@
 function showMenu() {
-    var menu = document.getElementsByClassName("menuItem")
-    if (menu) {
-   if (menu.style.display == "none") {
-        menu.style.display = "block";
-   } else {
-       menu.style.display = "none";
-   }
-    } else {
-        alert("Failed");
+    var menu = document.getElementsByClassName("menuItem");
+    for (var i = 0; i < menu.length; i++) {
+        if (menu[i].style.display == "none") {
+                menu[i].style.display = "block";
+        } else {
+            menu[i].style.display = "none";
+        }
+    }
+  
+}
+
+//Show list of new products when user click on "Sản phẩm mới về" on menu bar
+function showNewProducts() {
+    var listPro = document.getElementsByClassName("newProducts");
+    var bestSeller = document.getElementsByClassName("bestSellers");
+    var hightest = document.getElementsByClassName("highestView");
+    for (var i = 0; i <listPro.length; i++) {
+        //Hidden list of best seller products and highest view products
+        if (bestSeller[i].style.display != "none") {
+            bestSeller[i].style.display = "none";
+        } 
+        if (hightest[i].style.display != "none") {
+            hightest[i].style.display = "none";
+        } 
+
+        //Show list of new products
+        listPro[i].style.display = (listPro[i].style.display == "none") ? "block" : "none";
+
+    }
+}
+
+//Show list of best seller products when user click on "Sản phẩm bán chạy nhất" on menu bar
+function showBestSellers() {
+    var listPro = document.getElementsByClassName("bestSellers");
+    var newPro = document.getElementsByClassName("newProducts");
+    var hightest = document.getElementsByClassName("highestView");
+    for (var i = 0; i <listPro.length; i++) {
+        //Hidden list of new products and highest view products
+        if (newPro[i].style.display != "none") {
+            newPro[i].style.display = "none";
+        } 
+        if (hightest[i].style.display != "none") {
+            hightest[i].style.display = "none";
+        } 
+        
+        //SHow lít of best seller products
+        listPro[i].style.display = (listPro[i].style.display == "none") ? "block" : "none";
+    }
+}
+
+//Show list of products having highest view when user click on "Sản phẩm có lượt xem nhiều nhất" on menu bar
+function showHighestViewPro() {
+    var listPro = document.getElementsByClassName("highestView");
+    var bestSeller = document.getElementsByClassName("bestSellers");
+    var newPro = document.getElementsByClassName("newProducts");
+    for (var i = 0; i <listPro.length; i++) {
+       //Hidden list of new products and highest view products
+       if (newPro[i].style.display != "none") {
+        newPro[i].style.display = "none";
+        } 
+        if (bestSeller[i].style.display != "none") {
+            bestSeller[i].style.display = "none";
+        } 
+    
+    //SHow lít of best seller products
+        listPro[i].style.display = (listPro[i].style.display == "none") ? "block" : "none";
     }
 }
