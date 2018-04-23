ac674dbaf6721bf0ef96c082603bd4c3d7200df0
An <1512012@student.hcmus.edu.vn>
2018-04-24 02:17:36 +0700

    [AdminPage]Create adding new products form

--- assets/js/javaScript_AdminPage.js
+++ assets/js/javaScript_AdminPage.js
@@ -1,9 +1,30 @@
+//**************************  QUẢN LÝ SẢN PHẨM *********************/
+//Hiển thị menu để lựa chọn danh sách sản phẩm
 function showOptionToViewProduct() {
     var listOption = document.getElementsByClassName("detailOption")[0];
     if (listOption.style.display == "none") {
         listOption.style.display = "block";
     } else {
         listOption.style.display = "none";
-    }
-    
+    }    
+}
+
+function addNewProduct() {
+    var formToAddNew = document.getElementsByClassName("addNewForm")[0];
+    //if (formToAddNew.style.display == "none") {
+        formToAddNew.style.display = "block";
+
+}
+
+function closeFormAddNew() {
+    var formToAddNew = document.getElementsByClassName("addNewForm")[0];
+    //if (formToAddNew.style.display == "none") {
+        formToAddNew.style.display = "none";
+}
+
+
+
+//********************************  ĐĂNG XUẤT ***************************/
+function logout() {
+    '<h2>KFEwjfoiejwf</h2>';
 }
