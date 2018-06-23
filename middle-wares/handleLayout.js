20e81f2e80f10c9628788f70f0b584a0fc3ca530
vttan4397 <1512013@student.hcmus.edu.vn>
2018-06-23 22:10:09 +0700

    Update with nodemon

--- middle-wares/handleLayout.js
+++ middle-wares/handleLayout.js
@@ -3,6 +3,10 @@ var productRepo = require('../repos/productRepo');
 module.exports = (req, res, next) => {
 
     productRepo.loadAll().then(rows => {
+    	rows.sort((a,b)=>b.productSold - a.productSold);
+    	console.log(rows);
+
+
         res.locals.layoutVM = {
             products: rows,
         }
